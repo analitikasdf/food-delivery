f3f86e178f2d24dea466bac7bb0f0c9285431f2a
Barsukov D <b.dmitry@alef.im>
2022-12-22 00:02:07 +0400

    add sliders

---
+++ src/components/MainSlide.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="w-auto h-56 flex items-center justify-center bg-gray-700">
+		<h2 class="text-white text-2xl">{{ title }}</h2>
+	</div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+	title: string;
+}
+
+const props = defineProps<Props>();
+</script>
---
+++ src/components/MainSliders.vue
@@ -0,0 +1,28 @@
+<template>
+	<swiper :slides-per-view="1" :space-between="50" navigation :modules="modules" :pagination="{ clickable: true }">
+		<swiper-slide v-for="(slide, index) in slides" :key="index">
+			<MainSlideVue :title="slide.title" />
+		</swiper-slide>
+	</swiper>
+</template>
+
+<script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css';
+
+import { Navigation, Pagination } from 'swiper';
+import MainSlideVue from './MainSlide.vue';
+const modules = [Navigation, Pagination];
+
+const slides: mainSlides[] = [{ title: 'Новинка 1' }, { title: 'Новинка 2' }, { title: 'Новинка 3' }];
+//TODO типизация
+
+interface mainSlides {
+	title: string;
+}
+
+// Import Swiper styles
+</script>
